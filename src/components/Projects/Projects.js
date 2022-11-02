9349e97745f92bbc5faeb2078a6d111cbe732ca9
arthur1997-wq <ajashi.shadow@gmail.com>
2022-11-02 19:43:04 +0100

    fix images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -51,7 +51,7 @@ const Projects = () => {
 			imageSrc: ecommerceImg,
 			description:"est un site web E-LEARNING offre tous les types des cours du secteur de developpement informatique. Vous trouverez dans se site vitrine virtuelle toutes les meilleures cours de programmation",
 			stack:['JAVA', 'SPRING BOOT', 'BootStrap'],
-			thumbnail :[blogImage,blogImage,blogImage,blogImage],
+			thumbnail :[],
 			descGenral:"Voluptate cillum minim qui tempor anim fugiat ea velit do quis. Nostrud ipsum amet veniam officia minim est occaecat ad. Veniam minim exercitation sit mollit laboris. Elit qui veniam est culpa esse consequat ipsum ad sit minim consectetur ea ex deserunt. Consequat culpa dolore mollit fugiat incididunt. Ut adipisicing eiusmod proident pariatur anim non. Ipsum deserunt occaecat consectetur minim anim ipsum pariatur adipisicing consequat."
 
 		},
@@ -62,7 +62,7 @@ const Projects = () => {
 			imageSrc: chatAppImg,
 			description:"est un site web de chat offre une tous les types des cours du secteur de developpement informatique. Vous trouverez dans se site vitrine virtuelle toutes les meilleures cours de programmation ",
 			stack:['Python', 'Flask',"Socket.io", 'BootStrap'],
-			thumbnail :[blogImage,blogImage,blogImage,blogImage],
+			thumbnail :[],
 			descGenral:"Voluptate cillum minim qui tempor anim fugiat ea velit do quis. Nostrud ipsum amet veniam officia minim est occaecat ad. Veniam minim exercitation sit mollit laboris. Elit qui veniam est culpa esse consequat ipsum ad sit minim consectetur ea ex deserunt. Consequat culpa dolore mollit fugiat incididunt. Ut adipisicing eiusmod proident pariatur anim non. Ipsum deserunt occaecat consectetur minim anim ipsum pariatur adipisicing consequat."
 
 		},
@@ -73,7 +73,7 @@ const Projects = () => {
 			imageSrc: netflixCloneImg,
 			description:"GESTION D'HOPITAL est un logiciel développé sous ASP.Net et Sql Server est destiné aux cliniques et cabinets médicaux désireux de connaître les situations des recettes issues des prestations réalisées pour les patients ainsi que les medecins. Comme dit le logiciel est donc composé de deux modules: Gestion des patients et Gestion de Medecins. Ces deux modules sont indépendants l'un de l'autre.",
 			stack:['ASP.NET', 'Bootsrtap 5', 'SQLSERVER'],
-			thumbnail :[blogImage,blogImage,blogImage,blogImage],
+			thumbnail :[],
 			descGenral:"Voluptate cillum minim qui tempor anim fugiat ea velit do quis. Nostrud ipsum amet veniam officia minim est occaecat ad. Veniam minim exercitation sit mollit laboris. Elit qui veniam est culpa esse consequat ipsum ad sit minim consectetur ea ex deserunt. Consequat culpa dolore mollit fugiat incididunt. Ut adipisicing eiusmod proident pariatur anim non. Ipsum deserunt occaecat consectetur minim anim ipsum pariatur adipisicing consequat."
 
 		},
